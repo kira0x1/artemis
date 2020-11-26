0ce4f6575070a47533237dfb22d1cba72d16456d
Kira <kira.eva@protonmail.com>
2020-11-26 06:46:28 +0300

    testing with admin perms

--- src/commands/ungrouped/test.ts
+++ src/commands/ungrouped/test.ts
@@ -4,7 +4,7 @@ export const command: Command = {
     name: 'Test',
     description: 'Test command',
     aliases: ['t'],
-    perms: [],
+    perms: ['ADMINISTRATOR'],
 
     execute(message, args) {
         message.channel.send('meow')
