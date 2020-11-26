f8a5054c6dcef474ec19c226b68753af79222e92
Kira <kira.eva@protonmail.com>
2020-11-26 06:53:22 +0300

    removed accedintal express import

--- src/commands/moderation/userinfo.ts
+++ src/commands/moderation/userinfo.ts
@@ -1,27 +1,26 @@
-import { query } from "express";
-import { Command, FindMember } from "../../util/command-util";
-import { createFooter, QuickEmbed } from "../../util/styleUtil";
+import { Command, FindMember } from '../../util/command-util'
+import { createFooter, QuickEmbed } from '../../util/styleUtil'
 
 export const command: Command = {
-  name: "UserInfo",
-  description: "Retrieve information about a user",
-  aliases: ["uinfo"],
-  args: true,
-  usage: "[ID | TAG | NAME]",
+   name: 'UserInfo',
+   description: 'Retrieve information about a user',
+   aliases: ['uinfo'],
+   args: true,
+   usage: '[ID | TAG | NAME]',
 
-  async execute(message, args) {
-    const query = args.join(" ");
-    const target = await FindMember(message, query);
-    if (!target) return QuickEmbed(message, `User ${query} not found`);
+   async execute(message, args) {
+      const query = args.join(' ')
+      const target = await FindMember(message, query)
+      if (!target) return QuickEmbed(message, `User ${query} not found`)
 
-    const embed = createFooter(message)
-      .setTitle("User info")
-      .setDescription(`User info for ${target}`)
-      .setThumbnail(target.user.avatarURL({ dynamic: true, size: 4096 }))
-      .addField("User ID", `\`${target.id}\``)
-      .addField("Created at", target.user.createdAt.toUTCString())
-      .addField("Joined at", target.joinedAt.toUTCString());
+      const embed = createFooter(message)
+         .setTitle('User info')
+         .setDescription(`User info for ${target}`)
+         .setThumbnail(target.user.avatarURL({ dynamic: true, size: 4096 }))
+         .addField('User ID', `\`${target.id}\``)
+         .addField('Created at', target.user.createdAt.toUTCString())
+         .addField('Joined at', target.joinedAt.toUTCString())
 
-    message.channel.send(embed);
-  },
-};
+      message.channel.send(embed)
+   },
+}
